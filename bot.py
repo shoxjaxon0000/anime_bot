31b4d8dec093f42bab4180ea122a2e5a68989c17
shoxjaxon0000 <113027729+shoxjaxon0000@users.noreply.github.com>
2026-06-17 13:06:44 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -37,7 +37,7 @@ BOT_USERNAME = "@Animez1_bot"
 MAJBURIY_KANALLAR = [
     {
         "channel_id": -1001234567890,          # ← o'zingizning kanal ID'ingizni kiriting
-        "url": "https://t.me/your_channel",     # ← obuna havolasi
+        "url": "https://t.me/Animeztv1",     # ← obuna havolasi
         "name": "📢 Kanalimiz",                  # ← tugma nomi
     },
 ]
